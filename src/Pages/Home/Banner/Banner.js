fb6f9f56418a4be1bba0d9d6b1b8134dde1af11c
raihan512 <raihangazi1024@gmail.com>
2023-03-05 04:49:26 +0600

    banner dynamically controlling with fake data part 5 done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,75 +1,27 @@
 import React from "react";
+import {} from "./banner.css";
 import img1 from "../../../assets/images/banner/1.jpg";
 import img2 from "../../../assets/images/banner/2.jpg";
 import img3 from "../../../assets/images/banner/3.jpg";
 import img4 from "../../../assets/images/banner/4.jpg";
 import img5 from "../../../assets/images/banner/5.jpg";
 import img6 from "../../../assets/images/banner/6.jpg";
+import BannerItem from "./BannerItem/BannerItem";
 
 const Banner = () => {
+  const banner = [
+    { id: 1, img: img1, prev: 6, next: 2 },
+    { id: 2, img: img2, prev: 1, next: 3 },
+    { id: 3, img: img3, prev: 2, next: 4 },
+    { id: 4, img: img4, prev: 3, next: 5 },
+    { id: 5, img: img5, prev: 4, next: 6 },
+    { id: 6, img: img6, prev: 5, next: 1 },
+  ];
   return (
     <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full" alt="" />
-        <div className="absolute flex justify-end transform -translate-y-1/2 top-2/4 left-5">
-          <h3 className="text-6xl text-white font-bold">
-            Affordable <br /> Price For Car <br /> Servicing
-          </h3>
-        </div>
-        <div className="absolute flex justify-end transform w-2/5 -translate-y-1/2 top-3/4 left-5">
-          <p className="text-white text-xl">
-            There are many variations of passages of available, but the majority
-            have suffered alteration in some form
-          </p>
-        </div>
-        <div className="absolute flex justify-start transform -translate-y-2/4 top-3/4 left-5 mt-20">
-          <button className="btn btn-warning mr-5 rounded-none">Warning</button>
-          <button className="btn btn-warning btn-outline rounded-none">
-            Warning
-          </button>
-        </div>
-        <div className="absolute flex justify-end transform right-5 bottom-2">
-          <a href="#slide4" className="btn rounded-none mr-2 text-white">
-            ❮
-          </a>
-          <a href="#slide2" className="btn rounded-none text-white">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} className="w-full" alt="" />
-        <div className="absolute flex justify-end transform right-5 bottom-2">
-          <a href="#slide1" className="btn rounded-none mr-2 text-white">
-            ❮
-          </a>
-          <a href="#slide3" className="btn rounded-none text-white">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={img3} className="w-full" alt="" />
-        <div className="absolute flex justify-end transform right-5 bottom-2">
-          <a href="#slide2" className="btn rounded-none mr-2 text-white">
-            ❮
-          </a>
-          <a href="#slide4" className="btn rounded-none text-white">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={img4} className="w-full" alt="" />
-        <div className="absolute flex justify-end transform right-5 bottom-2">
-          <a href="#slide3" className="btn rounded-none mr-2 text-white">
-            ❮
-          </a>
-          <a href="#slide1" className="btn rounded-none text-white">
-            ❯
-          </a>
-        </div>
-      </div>
+      {banner.map((banner) => (
+        <BannerItem key={banner.id} banner={banner}></BannerItem>
+      ))}
     </div>
   );
 };
