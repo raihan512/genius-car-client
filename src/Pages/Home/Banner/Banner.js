dec8d0c1a5b8289d241d7323ece758837d8f876d
raihan512 <raihangazi1024@gmail.com>
2023-03-05 04:30:20 +0600

    polished banner in home page part 4

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -11,44 +11,61 @@ const Banner = () => {
     <div className="carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
+        <div className="absolute flex justify-end transform -translate-y-1/2 top-2/4 left-5">
+          <h3 className="text-6xl text-white font-bold">
+            Affordable <br /> Price For Car <br /> Servicing
+          </h3>
+        </div>
+        <div className="absolute flex justify-end transform w-2/5 -translate-y-1/2 top-3/4 left-5">
+          <p className="text-white text-xl">
+            There are many variations of passages of available, but the majority
+            have suffered alteration in some form
+          </p>
+        </div>
+        <div className="absolute flex justify-start transform -translate-y-2/4 top-3/4 left-5 mt-20">
+          <button className="btn btn-warning mr-5 rounded-none">Warning</button>
+          <button className="btn btn-warning btn-outline rounded-none">
+            Warning
+          </button>
+        </div>
+        <div className="absolute flex justify-end transform right-5 bottom-2">
+          <a href="#slide4" className="btn rounded-none mr-2 text-white">
             ❮
           </a>
-          <a href="#slide2" className="btn btn-circle">
+          <a href="#slide2" className="btn rounded-none text-white">
             ❯
           </a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={img2} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
+        <div className="absolute flex justify-end transform right-5 bottom-2">
+          <a href="#slide1" className="btn rounded-none mr-2 text-white">
             ❮
           </a>
-          <a href="#slide3" className="btn btn-circle">
+          <a href="#slide3" className="btn rounded-none text-white">
             ❯
           </a>
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={img3} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
+        <div className="absolute flex justify-end transform right-5 bottom-2">
+          <a href="#slide2" className="btn rounded-none mr-2 text-white">
             ❮
           </a>
-          <a href="#slide4" className="btn btn-circle">
+          <a href="#slide4" className="btn rounded-none text-white">
             ❯
           </a>
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={img4} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
+        <div className="absolute flex justify-end transform right-5 bottom-2">
+          <a href="#slide3" className="btn rounded-none mr-2 text-white">
             ❮
           </a>
-          <a href="#slide1" className="btn btn-circle">
+          <a href="#slide1" className="btn rounded-none text-white">
             ❯
           </a>
         </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,7 +4,7 @@ import logo from "../../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
+    <footer className="footer p-10 bg-base-200 text-white">
       <div>
         <img src={logo} alt="" />
         <p>
